e2d0bc9534e86c538596707b16283dff4ed9496d
Chris Couzens <ccouzens@gmail.com>
2024-12-29 18:44:43 +0000

    Add initial card component and story
    There is an interesting bug in storybook where I cannot select the king
    card. Also, all the dropdown entries are off by one.

---
+++ src/Card.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  type Suit = "clubs" | "diamonds" | "hearts" | "spades";
+  type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13;
+  const cardBackChar = "🂠";
+  const suitChars: Record<Suit, number> = {
+    clubs: 0x1f0d0,
+    diamonds: 0x1f0c0,
+    hearts: 0x1f0b0,
+    spades: 0x1f0a0,
+  };
+
+  const toUnicode = ({ suit, rank }: { suit: Suit; rank: Rank }) => {
+    const ace = suitChars[suit];
+    return String.fromCodePoint(ace + rank);
+  };
+
+  interface Props {
+    /** The suit of the card */
+    suit: Suit;
+    /** The rank of the card. Aces are 1 and Kings are 13 */
+    rank: Rank;
+    /** Is the rank and suit visible to the user (`true`) or is the back of the card visible to the user (`false`). */
+    faceUp: boolean;
+  }
+
+  const { suit, rank, faceUp }: Props = $props();
+  const char = $derived(faceUp ? toUnicode({ suit, rank }) : cardBackChar);
+</script>
+
+<div class="card">{char}</div>
+
+<style>
+</style>
---
+++ src/stories/Card.stories.svelte
@@ -0,0 +1,23 @@
+<script module lang="ts">
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import Card from "./../Card.svelte";
+  import { fn } from "@storybook/test";
+
+  const { Story } = defineMeta({
+    component: Card,
+    tags: ["autodocs"],
+    argTypes: {
+      suit: {
+        control: { type: "select" },
+      },
+      rank: {
+        control: { type: "select" },
+      },
+    },
+    args: {
+      faceUp: true,
+    },
+  });
+</script>
+
+<Story name="Ace of Spades" args={{ rank: 1, suit: "spades" }} />
