98d3de1e8ae9a4f77f321045d027619063b8ab69
Chris Couzens <ccouzens@gmail.com>
2025-01-26 18:50:53 +0000

    Unused imports

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-import Counter from "./lib/Counter.svelte";
 import Game from "./Game.svelte";
-import { cards } from "./Card";
 import { newGame } from "./Game";
 const game = newGame();
 </script>
